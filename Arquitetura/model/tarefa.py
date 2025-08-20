900aada0ea541d4c6320881a8721bae187c98237
ArthurAdrianoMM <139277319+ArthurAdrianoMM@users.noreply.github.com>
2025-08-20 09:48:14 -0300

    Delete Arquitetura directory

--- Arquitetura/model/tarefa.py
+++
@@ -1,6 +0,0 @@
-class Tarefa:
-    def __init__(self, descricao: str):
-        self.descricao = descricao.strip()  # tira espaços extras
-
-    def __str__(self):
-        return self.descricao
--- Arquitetura/services/persistencia.py
+++
@@ -1,7 +0,0 @@
-class PersistenciaArquivo:
-    def __init__(self, nome_arquivo: str):
-        self.nome_arquivo = nome_arquivo
-
-    def salvar_em_arquivo(self, tarefa):
-        with open(self.nome_arquivo, "a", encoding="utf-8") as f:
-            f.write(str(tarefa) + "\n")
